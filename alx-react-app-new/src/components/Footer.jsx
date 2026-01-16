460aceb9fa351484c80248b7ccaecbb63f730cae
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-01-16 18:14:17 +0100

    committed

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer
+        style={{
+          fontSize: "15px",
+          textAlign: "center",
+          marginTop: "20px",
+          color: "gray",
+        }}
+      >
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <>
+      <main
+        style={{
+          color: "white",
+          fontSize: "20px",
+          backgroundColor: "green",
+          alignContent: "center",
+          padding: "10px",
+          margin: "auto",
+        }}
+      >
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </>
+  );
+}
+
+export default MainContent;
