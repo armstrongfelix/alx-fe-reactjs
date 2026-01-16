ff4bed19c1db8e9ac1c5ec61a8115ea01cdcc90b
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-01-16 22:48:38 +0100

    commmitted

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,7 @@
+// import the createContext function from the 'react' library
+import { createContext } from "react";
+// initailize the UserContext
+// create a UserContext using createContext and export it
+const UserContext = createContext();
+// export the UserContext
+export default UserContext;
