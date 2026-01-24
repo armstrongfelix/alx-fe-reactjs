6655afe56451ffdb636f55e23069a43b4cd2ecd4
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-01-24 14:12:37 +0100

    week11 assignment 02 added

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,15 +1,31 @@
 import { useRecipeStore } from "./recipeStore";
+import { Link } from "react-router-dom"; // Import Link
 
 const RecipeList = () => {
+  // Use filteredRecipes to ensure search functionality works
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
-    <div>
+    <div className="recipe-list">
       {filteredRecipes.length > 0 ? (
         filteredRecipes.map((recipe) => (
-          <div key={recipe.id}>
+          <div
+            key={recipe.id}
+            style={{
+              border: "1px solid #ddd",
+              margin: "10px",
+              padding: "10px",
+            }}
+          >
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
+            {/* Add the Link component here */}
+            <Link
+              to={`/recipe/${recipe.id}`}
+              style={{ color: "blue", textDecoration: "underline" }}
+            >
+              View Details
+            </Link>
           </div>
         ))
       ) : (
@@ -18,4 +34,5 @@ const RecipeList = () => {
     </div>
   );
 };
+
 export default RecipeList;
