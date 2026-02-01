0227c3922d144027cb2f1595f1b1ee51a681500e
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-02-01 15:05:54 +0100

    committed integrated tailwind css

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const fetchAdvancedUserData = async (username, location, minRepos) => {
-  let query = `q=${username}`;
+  let query = `user:${username}`; // Basic username search
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
   const response = await axios.get(
-    `https://api.github.com/search/users?${query}`,
+    `https://api.github.com/search/users?q=${query}`,
   );
-  return response.data; // Note: Search API returns an object with an 'items' array
+  return response.data; // This returns an array in 'items'
 };
