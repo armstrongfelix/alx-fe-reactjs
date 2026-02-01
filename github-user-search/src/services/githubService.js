eb0280f3c9658767e72f23c02fff73b1408b68c9
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-02-01 15:23:51 +0100

    committed

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-export const fetchAdvancedUserData = async (username, location, minRepos) => {
-  let query = `user:${username}`; // Basic username search
+/**
+ * Fetches users based on username, location, and minimum repository count.
+ * Endpoint: https://api.github.com/search/users?q={query}
+ */
+export const fetchUserData = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username}`;
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
   const response = await axios.get(
     `https://api.github.com/search/users?q=${query}`,
   );
-  return response.data; // This returns an array in 'items'
+  return response.data;
 };
