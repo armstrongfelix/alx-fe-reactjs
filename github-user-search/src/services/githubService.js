6313dbf38108c07f3916e2d8676d5ea189927699
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-02-01 14:56:12 +0100

    committed modified API services

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+export const fetchAdvancedUserData = async (username, location, minRepos) => {
+  let query = `q=${username}`;
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
+
+  const response = await axios.get(
+    `https://api.github.com/search/users?${query}`,
+  );
+  return response.data; // Note: Search API returns an object with an 'items' array
 };
